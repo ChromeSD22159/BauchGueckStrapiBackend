752d7325427aaa8ddb641c2140229dd9caf3a1f3
ChromeSD22159 <65664594+ChromeSD22159@users.noreply.github.com>
2024-08-20 00:07:35 +0200

    countdown-timers/update-or-insert route

--- src/api/countdown-timer/routes/countdown-timer.js
+++ src/api/countdown-timer/routes/countdown-timer.js
@@ -7,3 +7,16 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::countdown-timer.countdown-timer');
+
+module.exports = {
+    routes: [
+      {
+        method: 'POST',
+        path: '/countdown-timers/update-or-insert',
+        handler: 'countdown-timer-update-or-insert.updateOrInsert',
+        config: {
+          policies: []
+        }
+      }
+    ]
+  }
